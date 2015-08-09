0505d28410ab584057e480c89599812006bd3f6f
wudi <wudi2852987@163.com>
2015-08-09 19:45:52 +0800

    update routes and remove package.json

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var fs = require('fs');
 var path = require('path');
 
+router.get('/', function(req, res) {
+    res.render('modules/pages/home/index');
+});
+
 router.get('/home', function(req, res) {
     res.render('modules/pages/home/index');
 });
