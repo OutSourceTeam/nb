1a8e76bf6811b8424291b05cbeecf168f081c5a7
kevin14 <kevin14@me.com>
2015-08-09 21:06:17 +0800

    新增page支持

--- public/modules/common/config.js
+++ public/modules/common/config.js
@@ -22,7 +22,8 @@ require.config({
         'poly' : 'packages/poly/poly.0.5.1.min',
         'less' : 'packages/less/1.7.0/less.min',
         'text': 'packages/require-text/2.0.14/text',
-        'common':'modules/common'
+        'common':'modules/common',
+        'pages':'modules/pages'
     },
     map: {
         '*': {
