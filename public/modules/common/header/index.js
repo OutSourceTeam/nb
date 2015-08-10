016dc9597bf4c7c9fcbaebb5cda5eb61e998437d
wudi <wudi2852987@163.com>
2015-08-10 23:14:56 +0800

    update routes

--- public/modules/common/header/index.js
+++ public/modules/common/header/index.js
@@ -1,4 +1,17 @@
 define(['less!./header'],function() {
-	
+
+    (function () {
+
+        $('.header_meun li').hover(function () {
+            $(this).addClass('hover_meun');
+        }, function () {
+            $(this).removeClass('hover_meun');
+        })
+
+
+    })()
+
+
+
 });
 
