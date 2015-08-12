b48bcf8e17c13168f9f011c8ec18133bec83c1cd
wudi <wudi2852987@163.com>
2015-08-13 07:29:49 +0800

    update header

--- public/modules/common/header/index.js
+++ public/modules/common/header/index.js
@@ -7,15 +7,34 @@ define(['less!./header'],function() {
             if(!$this.hasClass('runguwen')){
                 $this.children('span').addClass('hover_meun');
                 $this.children('span').children('a').addClass('acolor');
-                $this.children('.category_expand').show()
+                $this.children('.category_expand').fadeIn()
             }
         }, function () {
             var $this =$(this);
             $this.children('span').removeClass('hover_meun');
             $this.children('span').children('a').removeClass('acolor');
-            $this.children('.category_expand').hide()
+            $this.children('.category_expand').fadeOut()
         })
 
+
+        $(window).scroll(function () {
+            toTop()
+        });
+
+        $('#headerToTop').on('click', function () {
+            $(window).scrollTop(0);
+        })
+
+        function toTop(){
+            var body_scoll = $(window).scrollTop();
+
+            if(body_scoll>=55){
+                $('.header-tab').css({'top':'0px'}).slideDown().addClass('toTop');
+            }else{
+                $('.header-tab').css({'top':'-34px'}).slideUp().removeClass('toTop');
+            }
+        }
+        toTop()
 
     })()
 
