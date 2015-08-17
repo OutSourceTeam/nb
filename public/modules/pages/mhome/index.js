4739901da350729855530b008f67cc7783dbf271
zhiqingz <910202320@qq.com>
2015-08-17 23:36:29 +0800

    mobile首页

--- public/modules/pages/mhome/index.js
+++ public/modules/pages/mhome/index.js
@@ -6,5 +6,14 @@ define([
     'less!./mhome'
 ], function($, Swiper) {
     
-
+	var topBannerSwiper = new Swiper ('.topBannerSwiper', {
+        loop: true,
+        autoplay: 5000,
+        pagination: '.topBannerSwiper .swiper-pagination',
+        paginationClickable: true,
+        // paginationBulletRender: function (index, className) {
+        //     var iconName = "icon-circle-active";
+        //     return '<span class="' + className +" "+ iconName + '"></span>';
+        // }
+      });
 });
