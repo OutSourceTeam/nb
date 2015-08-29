42b7988be028afc2ad20622ebbeb423cc21ceb6b
wudi <wudi2852987@163.com>
2015-08-29 21:55:31 +0800

    update productlist

--- public/modules/pages/productList/index.js
+++ public/modules/pages/productList/index.js
@@ -31,35 +31,37 @@ define([
 
     $('.product').on('click', function (e) {
         var $item = $(this);
-        var prevHeight = $item.height();
-        if (ispropopeshou && (e.target.className == 'buyingGuide' || e.target.className == 'ablack')) {
-            return true;
-        } else if (ispropopeshou && e.target.className.indexOf('popeclosed') == -1) {
-            return false;
-        }
-        var $this = $(this)
 
         if (ispropopeshou && e.target.className.indexOf('popeclosed') != -1) {
-            var $propope = $this.find('.propope');
-                ispropopeshou = false
-                ispopeloading = false
-                ispopedone = false
-            var timing = 200;
-            $propope.animate({height: 0}, timing, function () {
-                $('#infoSwiper .swiper-button-prev').off();
-                $('#infoSwiper .swiper-button-next').off();
-                $('#imageSwiper .swiper-button-prev').off();
-                $('#imageSwiper .swiper-button-next').off();
-                $propope.remove();
-            });
-            $this.animate({'height': 284}, timing).removeClass('active');
-            $("body, html").animate({scrollTop: prevTop}, timing);
+            removePope();
             return false;
         }
+
+
+        if (ispropopeshou && (e.target.className == 'buyingGuide' || e.target.className == 'ablack')) {
+            return true;
+        } else if (ispropopeshou && (e.target.className=='maiginbtn'
+            || e.target.className=='imgpos'
+            || e.target.className=='prono'
+            || e.target.className=='pro'
+        )) {
+            removePope();
+            addPope($item,e);
+            return true;
+        }else if (ispropopeshou && e.target.className.indexOf('popeclosed') == -1) {
+            return false;
+        }
+
+        addPope($item,e)
+
+    })
+    function addPope($item,e) {
+        var $this = $item;
+        var prevHeight = $this.height();
         var proid = $this.data('id');
-         if(proid==undefined || proid ==null || proid == ""){
-             return false;
-         }
+        if (proid == undefined || proid == null || proid == "") {
+            return false;
+        }
         var phtml = '<div class="propope">' +
             '<div class="quickview-arrow"></div>' +
             '<div class="popeclosed icon-close-thin"></div>' +
@@ -74,57 +76,76 @@ define([
         var quickview = $propope.find('.quickview-arrow');
         var popeloading = $propope.find('.popeloading');
         quickview.css('left', pos);
-          $propope.show();
-         popeloading.show();
+        $propope.show();
+        popeloading.show();
 
-        getProductinfo(proid,$this,prevHeight,e,popeloading);
+        getProductinfo(proid, $this, prevHeight, e, popeloading);
+    }
 
-    })
 
-    function getProductinfo(id,$this,prevHeight,e,popeloading) {
+    function removePope(){
+        var $propope = $('.product').find('.propope');
+        ispropopeshou = false
+        ispopeloading = false
+        ispopedone = false
+        var timing = 200;
+        $propope.animate({height: 0}, timing, function () {
+            $('#infoSwiper .swiper-button-prev').off();
+            $('#infoSwiper .swiper-button-next').off();
+            $('#imageSwiper .swiper-button-prev').off();
+            $('#imageSwiper .swiper-button-next').off();
+            $propope.remove();
+        });
+        $propope.parent('li').animate({'height': 284}, timing).removeClass('active');
+        $("body, html").animate({scrollTop: prevTop}, timing);
+    }
+    function getProductinfo(id, $this, prevHeight, e, popeloading) {
         $.ajax({
             method: "GET",
-            url: 'http://test.newbalance.com.cn/index.php?s=/Home/Index/ajaxproductinfo/id/'+id
+            url: 'http://test.newbalance.com.cn/index.php?s=/Home/Index/ajaxproductinfo/id/' + id
         }).done(function (msg) {
             console.log(msg)
             popeloading.hide();
-            shouPope(msg,$this,prevHeight,e)
+            shouPope(msg, $this, prevHeight, e)
         }).fail(function (msg) {
             console.log(msg)
         });
     }
 
 
-    function shouPope(data,$this,prevHeight,e) {
+    function shouPope(data, $this, prevHeight, e) {
 
         var popedone = $this.find('.popedone');
         var imgpath = data.imgpath;
 
-        var data= data.data;
+        var data = data.data;
 
 
-        var titeimg ='',bigimg = '',buyingGuide='',isShowPrice='',tianmaolin='',jinDongLink='',yiHaoDianLink='';
-
-        $.each(data.colorList, function (n,item){
-            titeimg+= '<div class="swiper-slide" style="background-image:url('+imgpath+item.big_img+');"></div>'
-        })
-        $.each(data.colorImgList, function (n,item){
-            bigimg+= '<div class="swiper-slide" style="background-image:url('+imgpath+item.img+');"></div>'
-        })
-        if(data.isSelectionShoes=="1"){
-            buyingGuide ='<div><a class="buyingGuide" href="http://shoeadvisor.newbalance.com.cn" target="_blank">&nbsp;</a></div>';
+        var titeimg = '', bigimg = '', buyingGuide = '', isShowPrice = '', tianmaolin = '', jinDongLink = '', yiHaoDianLink = '';
+        if (data.colorList) {
+            $.each(data.colorList, function (n, item) {
+                titeimg += '<div class="swiper-slide" style="background-image:url(' + imgpath + item.big_img + ');"></div>'
+            })
         }
-        if(data.isShowPrice=="1"){
-            isShowPrice ='<span>￥'+data.price+'</span>';
+        if (data.colorImgList) {
+            $.each(data.colorImgList, function (n, item) {
+                bigimg += '<div class="swiper-slide" style="background-image:url(' + imgpath + item.img + ');"></div>'
+            })
         }
-        if(data.tianMaoLink && data.tianMaoLink!=""){
-            tianmaolin='<span><a href="'+data.tianMaoLink+'" target="_blank"><img  class="ablack" src="../../../images/tianmao.png" border="0" alt=""/></a></span>'
+        if (data.isSelectionShoes == "1") {
+            buyingGuide = '<div><a class="buyingGuide" href="http://shoeadvisor.newbalance.com.cn" target="_blank">&nbsp;</a></div>';
         }
-        if(data.jinDongLink && data.jinDongLink!=""){
-            jinDongLink='<span><a href="'+data.jinDongLink+'" target="_blank"><img  class="ablack" src="../../../images/jd.png" border="0" alt=""/></a></span>'
+        if (data.isShowPrice == "1") {
+            isShowPrice = '<span>￥' + data.price + '</span>';
         }
-        if(data.yiHaoDianLink && data.yiHaoDianLink!=""){
-            yiHaoDianLink='<span><a href="'+data.yiHaoDianLink+'" target="_blank"><img  class="ablack" src="../../../images/yhd.png" border="0" alt=""/></a></span>'
+        if (data.tianMaoLink && data.tianMaoLink != "") {
+            tianmaolin = '<span><a href="' + data.tianMaoLink + '" target="_blank"><img  class="ablack" src="../../../images/tianmao.png" border="0" alt=""/></a></span>'
+        }
+        if (data.jinDongLink && data.jinDongLink != "") {
+            jinDongLink = '<span><a href="' + data.jinDongLink + '" target="_blank"><img  class="ablack" src="../../../images/jd.png" border="0" alt=""/></a></span>'
+        }
+        if (data.yiHaoDianLink && data.yiHaoDianLink != "") {
+            yiHaoDianLink = '<span><a href="' + data.yiHaoDianLink + '" target="_blank"><img  class="ablack" src="../../../images/yhd.png" border="0" alt=""/></a></span>'
         }
 
         var protitlehtml = '<div class="protitle">' +
@@ -134,19 +155,19 @@ define([
             '<div class="popeswiper left">' +
             '<div class="popBannerBox">' +
             '<div class="swiper-container popeBannerSwiper" id="infoSwiper">' +
-            '<div class="swiper-wrapper">' +titeimg+'</div>' +
+            '<div class="swiper-wrapper">' + titeimg + '</div>' +
             '<div class="swiper-pagination"></div>' +
             '<div class="swiper-button-prev icon-arrow-left"></div>' +
             '<div class="swiper-button-next icon-arrow-right"></div>' +
-            '</div>'+'</div>' +buyingGuide +'</div>' +
+            '</div>' + '</div>' + buyingGuide + '</div>' +
             '<div class="popeswiper right">' +
             '<div class="poperemark">' +
-            '<div class="remarktitle">'+data.model + data.seriesSize + data.name+isShowPrice+'</div>' +
+            '<div class="remarktitle">' + data.model + data.seriesSize + data.name + isShowPrice + '</div>' +
             '<hr/>' +
-            '<div class="remarkcontent">' +data.introduction+'</div>' +
+            '<div class="remarkcontent">' + data.introduction + '</div>' +
             '<hr/>' +
             '<div class="remarkmore">' +
-            '<a class="ablack" href="'+data.mainProductsLink+'" target="_blank">更多详情＞</a>'+tianmaolin+jinDongLink+yiHaoDianLink+
+            '<a class="ablack" href="' + data.mainProductsLink + '" target="_blank">更多详情＞</a>' + tianmaolin + jinDongLink + yiHaoDianLink +
             '</div>' +
             '</div>' +
             '</div>' +
@@ -158,22 +179,24 @@ define([
 
         var popebananerhtml = '<div class="popebananer">' +
             '<div class="swiper-container popebigBannerSwiper" id="imageSwiper">' +
-            '<div class="swiper-wrapper">' +bigimg+'</div>' +
+            '<div class="swiper-wrapper">' + bigimg + '</div>' +
             '<div id="bigpagination" class="swiper-pagination"></div>' +
             '<div id="bigprev" class="swiper-button-prev icon-arrow-left"></div>' +
             '<div id="bignext" class="swiper-button-next icon-arrow-right"></div>' +
             '</div>' +
             '</div>';
 
+        if (data.colorImgList == null || data.colorImgList == undefined || (data.colorImgList.length && data.colorImgList.length == 0)) {
+            popebananerhtml = "";
+        }
 
-        popedone.append(protitlehtml+popebananerhtml);
+        popedone.append(protitlehtml + popebananerhtml);
 
-        showProdectinfo($this,data,prevHeight,e,imgpath)
+        showProdectinfo($this, data, prevHeight, e, imgpath)
     }
 
 
-
-    function showProdectinfo($this,data,prevHeight,e,imgpath){
+    function showProdectinfo($this, data, prevHeight, e, imgpath) {
         var $propope = $this.find('.propope');
         var popedone = $this.find('.popedone');
         var popeclosed = $this.find('.popeclosed');
@@ -208,10 +231,28 @@ define([
                 nextButton: '#infoSwiper .swiper-button-next',
                 prevButton: '#infoSwiper .swiper-button-prev',
                 paginationBulletRender: function (index, className) {
-                    return '<span class="' + className + '" style="background-image: url(' + imgpath+data.colorList[index].color_img + ')"></span>';
+                    var colorimg  = ""
+                    if (data.colorImgList == null || data.colorImgList == undefined || (data.colorImgList.length && data.colorImgList.length == 0)) {
+                        return false
+                    }
+                    if(data.colorList[index] && data.colorList[index].color_img){
+                        colorimg = data.colorList[index].color_img;
+                    }
+                    return '<span class="' + className + '" style="background-image: url(' + imgpath +colorimg+ ')"></span>';
                 },
+                onSlideNext: function(swiper){
+                    console.log(swiper)
+                    var curindex  = swiper.activeLoopIndex;
+                   var datas=  data.colorList[curindex];
+                    changeSwiperBigImg(datas,imgpath,popebigBannerSwiper)
+                },
+                onSlidePrev: function(swiper){
+                    var curindex  = swiper.activeLoopIndex;
+                    var datas=  data.colorList[curindex];
+                    changeSwiperBigImg(datas,imgpath,popebigBannerSwiper)
+                }
             });
-            popebigBannerSwiper = new Swiper('#imageSwiper', {
+           var  popebigBannerSwiper = new Swiper('#imageSwiper', {
                 loop: true,
                 speed: 300,
                 pagination: '#imageSwiper .swiper-pagination',
@@ -224,7 +265,14 @@ define([
                 prevButton: '#imageSwiper .swiper-button-prev'
             });
             $('#infoSwiper .swiper-pagination-switch').each(function (index, item) {
-                $(item).css('background-image', 'url(' + imgpath+data.colorList[index].color_img + ')');
+                var colorimg  = ""
+                if (data.colorImgList == null || data.colorImgList == undefined || (data.colorImgList.length && data.colorImgList.length == 0)) {
+                   return false
+                }
+                if(data.colorList[index] && data.colorList[index].color_img){
+                    colorimg = data.colorList[index].color_img;
+                }
+                $(item).css('background-image', 'url(' + imgpath+colorimg + ')');
             });
 
             // $('#imageSwiper .swiper-pagination-switch').each(function(index, item){
@@ -245,4 +293,29 @@ define([
             });
         }, 100);
     }
+
+    function changeSwiperBigImg(data,imgpath,popebigBannerSwiper){
+
+
+        var Oimg = $('#imageSwiper');
+
+        popebigBannerSwiper.removeAllSlides();
+
+
+
+        var imghtml = ''
+
+        if (data.colorImgList) {
+            $.each(data.colorImgList, function (n, item) {
+                var newSlide = popebigBannerSwiper.createSlide('<div   style="background-image:url(' + imgpath + item.img + ');"></div>','swiper-slide','div');
+                //imghtml += '<div class="swiper-slide" style="background-image:url(' + imgpath + item.img + ');"></div>'
+                newSlide.append(); //加到slides的最后
+
+            })
+        }
+
+
+    }
+
+
 });
