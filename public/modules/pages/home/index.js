a8a87db323b8edb0fd9e41b1a4d715b111e075eb
kevin14 <kevin14@me.com>
2015-08-30 11:15:06 +0800

    添加font

--- public/modules/pages/home/index.js
+++ public/modules/pages/home/index.js
@@ -6,33 +6,36 @@ define([
     'less!./home'
 ], function($, Swiper) {
 
-	var topBannerSwiper = new Swiper ('.topBannerSwiper', {
-        loop: true,
-        autoplay: 5000,
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
-        createPagination: true,
-        // paginationBulletRender: function (index, className) {
-        //     var iconName = "icon-polygon-active";
-        //     return '<span class="' + className +" "+ iconName + '"></span>';
-        // }
-      });
-      var bottomNewsSwiper = new Swiper ('.bottomNewsSwiper', {
+    if ($('.topBannerSwiper .swiper-wrapper .swiper-slide').length > 1) {
+        var topBannerSwiper = new Swiper('.topBannerSwiper', {
+            loop: true,
+            autoplay: 5000,
+            pagination: '.swiper-pagination',
+            paginationClickable: true,
+            createPagination: true,
+            // paginationBulletRender: function (index, className) {
+            //     var iconName = "icon-polygon-active";
+            //     return '<span class="' + className +" "+ iconName + '"></span>';
+            // }
+        });
+    };
+
+    var bottomNewsSwiper = new Swiper('.bottomNewsSwiper', {
         loop: true,
         // nextButton: '.swiper-button-next',
         // prevButton: '.swiper-button-prev',
-      });
-    $('.swiper-button-prev').on('click', function(){
+    });
+    $('.swiper-button-prev').on('click', function() {
         bottomNewsSwiper.swipePrev();
     });
-    $('.swiper-button-next').on('click', function(){
+    $('.swiper-button-next').on('click', function() {
         bottomNewsSwiper.swipeNext();
     });
-    $('.weixin').on('click', function(ev){
+    $('.weixin').on('click', function(ev) {
         $('.pageCover').addClass('show');
         ev.stopPropagation();
     });
-    $('.closeBtn').on('click', function(ev){
+    $('.closeBtn').on('click', function(ev) {
         $('.pageCover').removeClass('show');
         ev.stopPropagation();
     });
