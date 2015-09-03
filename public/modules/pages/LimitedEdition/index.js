053c45f1b22d2b00e2410fa94731740685acbf53
wudi <wudi2852987@163.com>
2015-09-03 09:27:27 +0800

    update limitionEdoitorn

--- public/modules/pages/LimitedEdition/index.js
+++ public/modules/pages/LimitedEdition/index.js
@@ -6,64 +6,67 @@ define([
 ], function ($) {
     var leftIsshow = false,rightIsshow=false;
     var banner = $('#limitedEdition-banner');
-
+    var toTop = $('.top-btn');
+    var isshowPanle = parseInt(getQueryString('isshowPanle'))|| 2;
     var left = banner.find('.left-panle')
     var leftBtn = left.find('.bnt')
     var leftlayer = left.find('.layer')
     var right = banner.find('.right-panle')
     var righttBtn = right.find('.bnt')
     var rightlayer = right.find('.layer')
+    var red = $('#red');
+
+    if(isshowPanle==2){
+        showPanle()
+    }else{
+        toTopCahng('gotop')
+    }
 
 
-    leftBtn.on('click',function () {
-        hasleftpanle('btn')
-    })
+    function showPanle(){
+        leftBtn.on('click',function () {
+            hasleftpanle('btn')
 
-    leftlayer.on('click',function () {
-        hasleftpanle('layer')
-    })
+        })
 
-
-    leftBtn.hover(function () {
-        leftlayer.css('opacity',.05)
-    }, function () {
-        leftlayer.css('opacity',0)
-    })
-    leftlayer.hover(function () {
-        leftlayer.css('opacity',.05)
-    }, function () {
-        leftlayer.css('opacity',0)
-    })
+        leftBtn.hover(function () {
+            leftlayer.css('opacity',.05)
+        }, function () {
+            leftlayer.css('opacity',0)
+        })
+        leftlayer.hover(function () {
+            leftlayer.css('opacity',.05)
+        }, function () {
+            leftlayer.css('opacity',0)
+        })
 
 
-    righttBtn.on('click',function () {
-        hasrightpanle('have')
-    })
-    //rightlayer.on('click',function () {
-    //    hasrightpanle('layer')
-    //})
+        righttBtn.on('click',function () {
+            hasrightpanle('have')
+        })
 
 
 
-    righttBtn.hover(function () {
-        rightlayer.css('opacity',.05)
-    }, function () {
-        rightlayer.css('opacity',0)
-    })
-    rightlayer.hover(function () {
-        rightlayer.css('opacity',.05)
-    }, function () {
-        rightlayer.css('opacity',0)
-    })
+
+        righttBtn.hover(function () {
+            rightlayer.css('opacity',.05)
+        }, function () {
+            rightlayer.css('opacity',0)
+        })
+        rightlayer.hover(function () {
+            rightlayer.css('opacity',.05)
+        }, function () {
+            rightlayer.css('opacity',0)
+        })
 
 
-    setBtnPos(banner)
-
-    $(window).on('resize', function () {
         setBtnPos(banner)
 
-    })
+        $(window).on('resize', function () {
+            setBtnPos(banner)
 
+        })
+    }
 
     function setBtnPos(banner) {
         var h = banner.find('.half-pic-bj').height();
@@ -91,6 +94,7 @@ define([
             right.hide()
             left.find('.full-pic-bj').show()
             leftIsshow=true
+            toTopCahng('showleftpanle','btn')
         }else if(type=='layer'){
             if(!leftIsshow) return false
             var $this = $(this);
@@ -104,10 +108,13 @@ define([
         }else if(type=='have'){
             $('body, html').css('overflow','auto')
             banner.hide()
-            $('#red').show();
+            red.show();
+            toTopCahng('showleftpanle','have')
+
         }
 
     }
+
     function hasrightpanle(type){
         if(type=='btn'){
             if(rightIsshow) return false
@@ -117,8 +124,9 @@ define([
             right.css('width','100%')
             rightlayer.stop(true).animate({width: '100%'}, 1000)
             left.hide()
-            right.find('.full-pic-bj').show()
+            right.find('.full-pic-bj').slideDown().show()
             rightIsshow=true
+            toTopCahng('showrightpanle','btn')
         }else if(type=='layer'){
             if(!rightIsshow) return false
             var $this = $(this);
@@ -132,8 +140,61 @@ define([
         }else if(type=='have'){
             $('body, html').css('overflow','auto')
             banner.hide()
-            $('#red').show();
+            red.show();
+            toTopCahng('showrightpanle','have')
         }
+
+    }
+    //根据 url 的名字 获得 值
+    function getQueryString(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null)return unescape(r[2]);
+        return null;
+    }
+
+    function toTopCahng(type,typename){
+        if(type=='showleftpanle' && typename=='btn'){
+            toTop.off()
+            toTop.show()
+            toTop.on('click', function () {
+                hasleftpanle('layer')
+                toTop.hide()
+            })
+
+        }else if(type=='showleftpanle' && typename=='have'){
+            toTop.off()
+            toTop.show()
+            toTop.on('click', function () {
+                $('body, html').css('overflow','hidden')
+                red.hide();
+                banner.show()
+                toTop.hide()
+            })
+        }else if(type=='showrightpanle' && typename=='btn'){
+            toTop.off()
+            toTop.show()
+            toTop.on('click', function () {
+                hasrightpanle('layer')
+                toTop.hide()
+            })
+        }else if(type=='showrightpanle' && typename=='have'){
+            toTop.off()
+            toTop.show()
+            toTop.on('click', function () {
+                $('body, html').css('overflow','hidden')
+                red.hide();
+                banner.show()
+                toTop.hide()
+
+            })
+        }else if(type=='gotop'){
+            $('body, html').css('overflow','initial')
+            banner.hide()
+            red.show()
+        }
+
+
 
     }
 
