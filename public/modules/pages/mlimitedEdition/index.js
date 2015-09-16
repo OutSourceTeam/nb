20643998ddd0d1b4d9e664a95211c5414935fb50
wudi <wudi2852987@163.com>
2015-09-16 13:58:09 +0800

    update  ld

--- public/modules/pages/mlimitedEdition/index.js
+++ public/modules/pages/mlimitedEdition/index.js
@@ -4,5 +4,33 @@ define([
     'less!./mlimitedEdition'
 ], function ($) {
 
+    var index = $('#index'),
+        jijiang = $('#jijiang'),
+        jidian = $('#jidian'),
+        jmore = index.find('.jmore'),
+        toTop = $('.top-btn'),
+        fmore = index.find('.fmore');
+        jmore.on('click', function () {
+            index.hide();
+            jidian.css('overflow','hidden').hide();
+            jijiang.show();
+            toTop.show()
+            jijiang.scrollTop(0);
+        });
+        fmore.on('click', function () {
+            index.hide();
+            jijiang.hide();
+            jidian.css('overflow','auto').show();
+            toTop.show()
+            jidian.scrollTop(0);
+        })
+
+        toTop.on('click', function () {
+            index.show();
+            jijiang.hide();
+            jidian.hide();
+            toTop.hide()
+        })
+
 
 });
